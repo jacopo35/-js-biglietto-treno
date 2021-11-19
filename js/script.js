f1644e2cdbb919f4a631095a7a6a7079368fb7cf
jacopo35 <jaczor00@gmail.com>
2021-11-19 15:32:25 +0100

    add-js&html -command

--- js/script.js
+++ js/script.js
@@ -7,3 +7,38 @@ va applicato uno sconto del 40 % per gli over 65.
 */
 
 
+// Chiedere all'utente il numero di chilometri che vuole percorrere
+
+
+let chilometri = parseFloat(prompt('Inserisci il numero di chilometri che vuoi percorrere'));
+console.log('Numero di chilometri da percorrere:', chilometri);
+
+// Chiedere all'utente la sua età
+
+
+let eta = parseInt(prompt('Inserisci la tua età'));
+console.log('Età del passeggero:', eta);
+
+// Sulla base di queste informazioni, calcolare il prezzo del biglietto
+
+let costoKm = 0.21;
+let prezzoBase = chilometri * costoKm;
+console.log('prezzoBase:', prezzoBase);
+
+// Prezzo finale per chi ha tra i 18 e 65 anni
+
+let prezzoFinale = prezzoBase;
+
+// Applicare uno sconto del 20% per i minorenni e del 40% per gli over 65
+
+if (eta < 18) {
+    prezzoFinale = (prezzoBase - ((prezzoBase * 20) / 100));
+    console.log('prezzoFinale', prezzoFinale);
+} else if (eta > 65) {
+    prezzoFinale = (prezzoBase - ((prezzoBase * 40) / 100));
+    console.log('prezzoFinale', prezzoFinale);
+}
+
+// L’output del prezzo finale va messo fuori in forma umana(con massimo due decimali, per indicare centesimi sul prezzo)
+
+document.getElementById('prezzofinale').innerHTML ='Il suo biglietto costa: ' + parseFloat(prezzoFinale).toFixed(2) + '€.';
